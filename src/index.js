9a168adeff24cec9d4f4c5e6b20b7e5b9085a41c
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-28 22:06:44 +0100

    initial set up

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import axios from 'axios';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
